b152160f4bcf283eb6466945f923b91a0dd629d9
LQKI <sander@saarend.com>
2016-08-15 23:24:35 +0300

    Add monk calculation

--- healcalculations/classes/monk.lua
+++ healcalculations/classes/monk.lua
@@ -1,12 +1,24 @@
 local E, L, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local TH = E:GetModule("TankHealth");
 
-local function GetArtifactMultiplier()
-    --unimplemented
-    return 1
-end
-
 function TH:Calculate_Monk()
-    --unimplemented
-    return 100000
+    -- Stat multipliers
+    local AP = UnitAttackPower("player")
+    local energy = UnitPower("player")
+    if pain < 15 then
+        return 0
+    end
+    local versatility = GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_DONE) + GetVersatilityBonus(CR_VERSATILITY_DAMAGE_DONE)
+    local versatilityMulti = 1 + (versatility / 100)
+
+    -- Healing spheres
+    local spheres = 0
+    if UnitBuff("player", "Healing Sphere") then
+        spheres = select(4, UnitBuff("player", "Healing Sphere"))
+    end
+
+    local singleSphereHeal = (7.5 * AP) * versatilityMulti
+    local totalHeal = singleSphereHeal * spheres
+
+    return math.ceil(totalHeal)
 end
