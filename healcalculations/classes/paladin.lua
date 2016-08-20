b7c04b09d29d0f6a6cd51d74c988a5e47892da0e
LQKI <sander@saarend.com>
2016-08-20 15:30:19 +0300

    Update paladin logic for 16/08 hotfix

--- healcalculations/classes/paladin.lua
+++ healcalculations/classes/paladin.lua
@@ -11,28 +11,31 @@ local function GetArtifactMultiplier()
 end
 
 function TH:Calculate_Paladin()
-    --get missing hp percentage
+    -- Get missing hp percentage
     local curHP = UnitHealth("player")
     local maxHP = UnitHealthMax("player")
     local missingHp = maxHP - curHP
+
+    -- LOTP heals for 35% of missing health
+    local healMulti = 0.35
 
     -- Stat multipliers
     local versatility = GetCombatRatingBonus(CR_VERSATILITY_DAMAGE_DONE) + GetVersatilityBonus(CR_VERSATILITY_DAMAGE_DONE)
     local versatilityMulti = 1 + (versatility / 100)
 
-    --check if standing in Consecration or Consecrated Hammer is skilled
-    local healMulti = ((select(4, GetTalentInfo(1,3,1))) or UnitBuff("player", GetSpellInfo(188370))) and 0.3 or 0.25
+    -- Check if standing in Consecration or Consecrated Hammer is skilled
+    local consMulti = ((select(4, GetTalentInfo(1,3,1))) or UnitBuff("player", GetSpellInfo(188370))) and 1.2 or 1
 
-    --check for Avenging Wrath
+    -- Check for Avenging Wrath
     local awMulti = UnitBuff("player", GetSpellInfo(31884)) and (1 + (select(17, UnitBuff("player", GetSpellInfo(31884)))) / 100) or 1
 
-    --check artifact traits
+    -- Check artifact traits
 
     local artifactMulti = 1 + 0.1 * currentRank
 
 
-    --multiply everything
-    local totalHeal = missingHp * healMulti * versatilityMulti * awMulti * artifactMulti
+    -- Multiply everything
+    local totalHeal = missingHp * healMulti * consMulti * versatilityMulti * awMulti * artifactMulti
 
     return totalHeal
 
